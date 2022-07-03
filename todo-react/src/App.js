1739817c2033551fed711f72d359c4fababce021
code4jhon <code4jhon@gmail.com>
2022-07-03 16:15:42 -0400

    Use useState hook to handle the state of the app.

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -3,18 +3,35 @@ import ButtonAppBar from './components/AppBar';
 import { TextField } from '@mui/material';
 import { Button } from '@mui/material';
 import TodoItem from './components/TodoItem'
+import { useState } from "react";
+
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+  const [tasks, setTasks] = useState(props.tasks);
+  const [name, setName] = useState('');
+
+
+  const taskList = tasks.map(task => (
     <TodoItem key={task.id} name={task.name} completed={task.completed}/>
   ));
+
+  const addNewTask = function () {
+    const uniqueId =  name + Math.random() + Math.random()
+    const newTask = { id: uniqueId, name: name, completed: false };
+
+    setTasks([...tasks, newTask]);
+  }
+
+  const onTextFieldChange = function (e) {
+    setName(e.target.value);
+  }
 
   return (
     <div>
       <ButtonAppBar></ButtonAppBar>
       <div className='app-body'>
-        <TextField id="outlined-basic" fullWidth label="What do I want to learn ?" variant="outlined" style={{'marginTop': '16px'}} />
-        <Button variant="contained" fullWidth component="span" style={{'margin': '8px 0 20px'}}>Add to the list</Button>
+        <TextField fullWidth label="What do I want to learn ?" variant="outlined" style={{'marginTop': '16px'}} onChange={onTextFieldChange}/>
+        <Button variant="contained" fullWidth component="span" style={{'margin': '8px 0 20px'}} onClick={addNewTask}>Add to the list</Button>
         {taskList}
       </div>
     </div>
