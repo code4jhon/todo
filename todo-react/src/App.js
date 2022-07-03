1e9174d53a583db0e66e8f1e72673b2da91933e2
code4jhon <code4jhon@gmail.com>
2022-07-03 16:44:26 -0400

    Implemented delete task functionality.

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -11,29 +11,47 @@ function App(props) {
   const [name, setName] = useState('');
 
 
-  const taskList = tasks.map(task => (
-    <TodoItem key={task.id} name={task.name} completed={task.completed}/>
-  ));
-
   const addNewTask = function () {
-    const uniqueId =  name + Math.random() + Math.random()
+    // TODO implement functionality that prevents adding duplicated entries (with the same name)
+    const uniqueId =  name + Math.random();
     const newTask = { id: uniqueId, name: name, completed: false };
 
-    setTasks([...tasks, newTask]);
+    setTasks([...tasks, newTask]); // updates the state of the app
   }
 
+  const deleteTask = function (taskName) {
+    // find the index of the task to delete
+    const index = tasks.findIndex(function (task) {
+      return task.name === taskName
+    })
+
+    if (index > -1) { // if there was an item found
+      const remainingTasks = [...tasks]
+
+      remainingTasks.splice(index, 1)
+      setTasks(remainingTasks) // update the state of the component
+    }
+  }
+
+  // uses state.task to create a list of todo items
+  const taskList = tasks.map(task => (
+    <TodoItem key={task.id} name={task.name} completed={task.completed} deleteTask={deleteTask}/>
+  ));
+
   const onTextFieldChange = function (e) {
-    setName(e.target.value);
+    setName(e.target.value); // call setName to keep state.name updated
   }
 
   return (
     <div>
       <ButtonAppBar></ButtonAppBar>
+
       <div className='app-body'>
         <TextField fullWidth label="What do I want to learn ?" variant="outlined" style={{'marginTop': '16px'}} onChange={onTextFieldChange}/>
         <Button variant="contained" fullWidth component="span" style={{'margin': '8px 0 20px'}} onClick={addNewTask}>Add to the list</Button>
         {taskList}
       </div>
+
     </div>
   );
 }
--- todo-react/src/components/TodoItem.js
+++ todo-react/src/components/TodoItem.js
@@ -1,9 +1,18 @@
 import { Checkbox, FormControlLabel } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function TodoItem(props) {
+  const onDeleteTask = function () {
+    props.deleteTask(props.name) // calls the function passed through the props, this function is defined in App.js
+  }
+
   return (
     <div>
       <FormControlLabel control={<Checkbox defaultChecked={props.completed}/>} label={props.name} />
+      <IconButton aria-label="delete" size="small" style={{position: 'absolute', right: '16px'}} onClick={onDeleteTask}>
+        <DeleteIcon fontSize="inherit" />
+      </IconButton>
     </div>
   )
 }
